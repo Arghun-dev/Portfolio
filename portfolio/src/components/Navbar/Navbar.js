46a7c2a9fc90f810e0334c3074af9491e015f4e5
Arghun-dev <arghun.developer@gmail.com>
2020-02-27 15:43:08 +0330

    Created Nav Links

---
+++ portfolio/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <ul className='Navbar-list'>
+            <li>About Me</li>
+            <li>My Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul> 
+    )
+}
+
+export default Navbar;
