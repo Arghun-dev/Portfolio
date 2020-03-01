39ff2fd0a448edf1fb5642922a1f3775d21d7a4a
Arghun-dev <arghun.developer@gmail.com>
2020-03-01 11:24:22 +0330

    Created gh-pages to deploy my website

--- portfolio/src/context/CardContext.js
+++ portfolio/src/context/CardContext.js
@@ -9,8 +9,7 @@ export default class CardProvider extends Component{
         this.state = {
             card: [
                 {id: 1, src: 'https://images.ctfassets.net/2y9b3o528xhq/5YhXXuS0hIw6JV3nJr3GgP/682bf2a70a98c3e466f26c2c2a812d65/front-end-web-developer-nanodegree--nd001.jpg', title: 'Basic'},
-                {id: 2, src: 'https://www.nylas.com/wp-content/uploads/React_Plugins@2x.png', title: 'Advance'},
-                {id: 3, src: 'https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/941461/910/607/m1/fpnw/wm1/108-.jpg?1454576210&s=5f8edcb420e18b0db1a74597837d4ff1', title: 'e-commerce website', address: 'http://world-shop.herokuapp.com/'}
+                {id: 2, src: 'https://www.nylas.com/wp-content/uploads/React_Plugins@2x.png', title: 'Advance'}
             ]
         }
     }
--- portfolio/src/pages/Projects/Projects.js
+++ portfolio/src/pages/Projects/Projects.js
@@ -7,20 +7,36 @@ import {CardContext} from '../../context/CardContext';
 
 const Projects = () => {
     const {card} = useContext(CardContext);
-    console.log(card);
     return (
         <div className='Projects'>
             <Navbar />
             <Link className='text-warning link' to='/myskills'>	&#8592;Back</Link>
             <h1>Projects</h1>
             <div className='Projects-content'>
-                <Card 
-                    card_number={card[2].id} 
-                    card_image={card[2].src} 
-                    card_title={card[2].title} 
-                    card_text={`My ${card[2].address}`}
-                    className='card'
-                />
+                <div className='card cardWidth'>
+                    <img className='card-img-top' src='https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/941461/910/607/m1/fpnw/wm1/108-.jpg?1454576210&s=5f8edcb420e18b0db1a74597837d4ff1' alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">e-commerce project</h5>
+                        <p className="card-text">Created with React and Redux</p>
+                        <a href="http://world-shop.herokuapp.com/" className="btn btn-primary">Continue</a>
+                    </div>     
+                </div>
+                <div className='card cardWidth'>
+                    <img className='card-img-top' src='https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/941461/910/607/m1/fpnw/wm1/108-.jpg?1454576210&s=5f8edcb420e18b0db1a74597837d4ff1' alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">Color App</h5>
+                        <p className="card-text">Created with React</p>
+                        <a href="http://world-shop.herokuapp.com/" className="btn btn-primary">Continue</a>
+                    </div>     
+                </div>
+                <div className='card cardWidth'>
+                    <img className='card-img-top' src='https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/941461/910/607/m1/fpnw/wm1/108-.jpg?1454576210&s=5f8edcb420e18b0db1a74597837d4ff1' alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">Todo App</h5>
+                        <p className="card-text">Created with React Hooks</p>
+                        <a href="http://world-shop.herokuapp.com/" className="btn btn-primary">Continue</a>
+                    </div>     
+                </div>
             </div>
         </div>
     )
