9bb024f20688f8097d1c50a263298e57c968fc59
Arghun-dev <arghun.developer@gmail.com>
2020-03-01 10:41:13 +0330

    Updates

---
+++ precache-manifest.5526558f31398f02193fbf8975e7a4dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f816216188990370082ad7ea4bf5f2e",
+    "url": "/Prtfolio/index.html"
+  },
+  {
+    "revision": "3c594b334c527a661e58",
+    "url": "/Prtfolio/static/css/main.e6ccb2e3.chunk.css"
+  },
+  {
+    "revision": "46bcc93b9a8178e0c2b2",
+    "url": "/Prtfolio/static/js/2.7e52777e.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/Prtfolio/static/js/2.7e52777e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c594b334c527a661e58",
+    "url": "/Prtfolio/static/js/main.3e321cbf.chunk.js"
+  },
+  {
+    "revision": "99b11d005865cf009123",
+    "url": "/Prtfolio/static/js/runtime-main.b1082bd9.js"
+  },
+  {
+    "revision": "9a2396016b92655f35609057d66b51e4",
+    "url": "/Prtfolio/static/media/background.9a239601.png"
+  }
+]);
